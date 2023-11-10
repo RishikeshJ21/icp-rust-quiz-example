89f1387df2cfcc7551754646caf6b4386ee1a2e2
Rishikesh Jadhav <74956080+RishikeshJ21@users.noreply.github.com>
2023-11-11 02:06:51 +0530

    Update lib.rs

--- src/icp_rust_boilerplate_backend/src/lib.rs
+++ src/icp_rust_boilerplate_backend/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, collections::HashMap};
+
 #[macro_use]
 extern crate serde;
 use candid::{Decode, Encode};
@@ -5,7 +7,6 @@ use ic_cdk::api::time;
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
 use ic_stable_structures::{BoundedStorable, Cell, DefaultMemoryImpl, StableBTreeMap, Storable};
 use std::{borrow::Cow, cell::RefCell};
-use std::collections::HashMap;
 
 type Memory = VirtualMemory<DefaultMemoryImpl>;
 type IdCell = Cell<u64, Memory>;
@@ -20,38 +21,50 @@ struct Quiz {
     updated_at: Option<u64>,
 }
 
-// a trait that must be implemented for a struct that is stored in a stable struct
 impl Storable for Quiz {
-    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+    fn to_bytes(&self) -> Cow<[u8]> {
         Cow::Owned(Encode!(self).unwrap())
     }
 
-    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
         Decode!(bytes.as_ref(), Self).unwrap()
     }
 }
 
-// another trait that must be implemented for a struct that is stored in a stable struct
 impl BoundedStorable for Quiz {
     const MAX_SIZE: u32 = 1024;
     const IS_FIXED_SIZE: bool = false;
 }
 
 thread_local! {
-        static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
-            MemoryManager::init(DefaultMemoryImpl::default())
-        );
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
+        MemoryManager::init(DefaultMemoryImpl::default())
+    );
 
-        static ID_COUNTER: RefCell<IdCell> = RefCell::new(
-            IdCell::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))), 0)
-                .expect("Cannot create a counter")
-        );
+    static ID_COUNTER: RefCell<IdCell> = RefCell::new(
+        IdCell::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))), 0)
+            .expect("Cannot create a counter")
+    );
 
-        static STORAGE: RefCell<StableBTreeMap<u64, Quiz, Memory>> =
-            RefCell::new(StableBTreeMap::init(
-                MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
-        ));
+    static STORAGE: RefCell<StableBTreeMap<u64, Quiz, Memory>> =
+        RefCell::new(StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
+    ));
+}
+
+impl Quiz {
+    fn new(id: u64, question: String, options: Vec<String>) -> Self {
+        let answers = options.iter().cloned().map(|option| (option, 0)).collect();
+        Self {
+            id,
+            question,
+            options,
+            answers,
+            created_at: time(),
+            updated_at: None,
+        }
     }
+}
 
 #[derive(candid::CandidType, Serialize, Deserialize, Default)]
 struct QuizPayload {
@@ -59,49 +72,39 @@ struct QuizPayload {
     options: Vec<String>,
 }
 
-
 #[ic_cdk::query]
 fn get_all_quiz() -> Result<Vec<Quiz>, Error> {
-    let quizzesMap : Vec<(u64, Quiz)> =  STORAGE.with(|service| service.borrow().iter().collect());
-    let length = quizzesMap.len();
-    let mut quizzes: Vec<Quiz> = Vec::new();
-    for key in 0..length {
-        quizzes.push(quizzesMap.get(key).unwrap().clone().1);
-    }
+    let quizzes: Vec<Quiz> = STORAGE.with(|service| service.borrow().values().cloned().collect());
 
-    if quizzes.len() > 0 {
+    if !quizzes.is_empty() {
         Ok(quizzes)
-    }else {
+    } else {
         Err(Error::NotFound {
-            msg: format!("There are currently no quiz"),
+            msg: "There are currently no quizzes".to_string(),
         })
     }
 }
 
-
 #[ic_cdk::query]
 fn get_quiz(id: u64) -> Result<Quiz, Error> {
-    match _get_quiz(&id) {
-        Some(message) => Ok(message),
-        None => Err(Error::NotFound {
-            msg: format!("a quiz with id={} not found", id),
-        }),
-    }
+    _get_quiz(&id).ok_or_else(|| Error::NotFound {
+        msg: format!("Quiz with id={} not found", id),
+    })
 }
 
 fn _get_quiz(id: &u64) -> Option<Quiz> {
     STORAGE.with(|s| s.borrow().get(id))
 }
 
-
 #[ic_cdk::update]
 fn create_quiz(payload: QuizPayload) -> Option<Quiz> {
-    let id = ID_COUNTER
-        .with(|counter| {
-            let current_value = *counter.borrow().get();
-            counter.borrow_mut().set(current_value + 1)
-        })
-        .expect("cannot increment id counter");
+    let id = ID_COUNTER.with(|counter| {
+        let current_value = *counter.borrow().get();
+        counter
+            .borrow_mut()
+            .set(current_value + 1)
+            .unwrap_or_else(|_| panic!("Cannot increment id counter"))
+    });
 
     let mut answers = HashMap::new();
 
@@ -109,36 +112,21 @@ fn create_quiz(payload: QuizPayload) -> Option<Quiz> {
         answers.insert(String::from(option), 0);
     }
 
-
-    let quiz = Quiz {
-        id,
-        question: payload.question,
-        options: payload.options,
-        answers,
-        created_at: time(),
-        updated_at: None,
-    };
+    let quiz = Quiz::new(id, payload.question, payload.options);
     do_insert(&quiz);
     Some(quiz)
 }
 
-
-// helper method to perform insert.
 fn do_insert(quiz: &Quiz) {
     STORAGE.with(|service| service.borrow_mut().insert(quiz.id, quiz.clone()));
 }
 
-
 #[ic_cdk::update]
 fn update_quiz(id: u64, payload: QuizPayload) -> Result<Quiz, Error> {
-
     let quiz_option: Option<Quiz> = STORAGE.with(|service| service.borrow().get(&id));
 
     match quiz_option {
-
         Some(mut quiz) => {
-
-
             let mut answers = HashMap::new();
 
             for option in &payload.options {
@@ -153,39 +141,27 @@ fn update_quiz(id: u64, payload: QuizPayload) -> Result<Quiz, Error> {
             Ok(quiz)
         }
         None => Err(Error::NotFound {
-            msg: format!(
-                "couldn't update a quiz with id={}. quiz not found",
-                id
-            ),
+            msg: format!("couldn't update a quiz with id={}. quiz not found", id),
         }),
     }
 }
-
 
 #[ic_cdk::update]
 fn delete_quiz(id: u64) -> Result<Quiz, Error> {
     match STORAGE.with(|service| service.borrow_mut().remove(&id)) {
         Some(quiz) => Ok(quiz),
         None => Err(Error::NotFound {
-            msg: format!(
-                "couldn't delete a quiz with id={}. quiz not found.",
-                id
-            ),
+            msg: format!("couldn't delete a quiz with id={}. quiz not found.", id),
         }),
     }
 }
 
-
 #[ic_cdk::update]
 fn answer_quiz(id: u64, option: String) -> Result<Quiz, Error> {
-
     let quiz_option: Option<Quiz> = STORAGE.with(|service| service.borrow().get(&id));
 
     match quiz_option {
-
         Some(mut quiz) => {
-
-            // Check if the selected option is valid
             if quiz.options.contains(&option) {
                 if let Some(answer_count) = quiz.answers.get_mut(&option) {
                     *answer_count += 1;
@@ -194,17 +170,13 @@ fn answer_quiz(id: u64, option: String) -> Result<Quiz, Error> {
                 do_insert(&quiz);
                 Ok(quiz)
             } else {
-                // Return an error if the selected option is not valid
                 Err(Error::NotFound {
                     msg: format!("The option '{}' is not found for this quiz.", option),
                 })
             }
         }
         None => Err(Error::NotFound {
-            msg: format!(
-                "couldn't cast a quiz with id={}. quiz not found",
-                id
-            ),
+            msg: format!("couldn't cast a quiz with id={}. quiz not found", id),
         }),
     }
 }
@@ -214,5 +186,4 @@ enum Error {
     NotFound { msg: String },
 }
 
-// need this to generate candid
 ic_cdk::export_candid!();
